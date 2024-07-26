078e1afe6a2c03bdb82cdd092139379fbcc10fa0
Kiky Dilfansyah <kikyrizkianunkywofficial@gmail.com>
2024-07-26 21:41:42 +0800

    js 26jul24 21:41

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-//Make the variabel object
+// Make the variable object
 const recipe = {
-    title : "Nasi Goreng (Fried Rice)",
-    instruction : "1. Heat oil in a pan. 2. Add garlic, shallots, and chili, sauté until fragrant. 3. Add chicken or shrimp, cook until done. 4. Add rice and stir-fry until heated through. 5. Add sweet soy sauce and salt, mix well. 6. Serve with fried egg on top.",
-    image : "https://i.ibb.co.com/k8BTQRV/079979700-1587487794-Sajiku-1.jpg",  
+    title: "Nasi Goreng (Fried Rice)",
+    instruction: "Heat oil in a pan. Add garlic, shallots, and chili, sauté until fragrant. Add chicken or shrimp, cook until done. Add rice and stir-fry until heated through. Add sweet soy sauce and salt, mix well. Serve with fried egg on top.",
+    image: "https://i.ibb.co.com/k8BTQRV/079979700-1587487794-Sajiku-1.jpg",
 };
 
-//Put to variable to connect to where will the script functioning an event and display the recipe
-const getRecipeButton = document.getElementById('getRecipeBtn'); //variable for button click
-const getRecipeContainer = document.getElementById('recipeContainer'); //variable for inserting the recipe
-
+// Put to variable to connect to where will the script function and display the recipe
+const getRecipeButton = document.getElementById('getRecipeBtn'); // variable for button click
+const getRecipeContainer = document.getElementById('recipeContainer'); // variable for inserting the recipe
 
 // Function to display the recipe 
 // content that will display
 function displayRecipe(recipe) {
-    recipeContainer.innerHTML = `
-    <h2 class="recipe-title">${recipe.title}</h2>
-    <img src="{recipe.image}" alt="${recipe.title} class="recipe-image">
-    <p class="recipe-instruction">${recipe.instruction}
+    // Hide the button after clicking
+    getRecipeButton.style.display = 'none';
+
+    //create order list for recipe
+    const instructionsArray = recipe.instruction.split('. ').filter(step => step.trim());
+    const instructionsList = instructionsArray.map(step => `<li>${step}</li>`).join('');
+
+    // Create HTML content for the recipe
+    getRecipeContainer.innerHTML = `
+    <h2 class="recipe-title" style="font-family: 'Calistoga', serif;">${recipe.title}</h2>
+    <img src="${recipe.image}" alt="${recipe.title}" class="recipe-image" style="width: 400px; height: auto;">
+    <ol class="recipe-instructions" style="text-align: justify">${instructionsList}</ol>
     `;
 }
 
-//Execute with event listener
+// Execute with event listener
 getRecipeButton.addEventListener('click', () => {
     displayRecipe(recipe);
 });
