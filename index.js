4d0c3af019109b7c2e55bfa8135377d18dba25d8
Kiky Dilfansyah <kikyrizkianunkywofficial@gmail.com>
2024-08-02 22:32:11 +0800

    js 2aug24 22:32

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Make the variable object
-const recipe = {
-    title: "Nasi Goreng (Fried Rice)",
-    instruction: "Heat oil in a pan. Add garlic, shallots, and chili, sauté until fragrant. Add chicken or shrimp, cook until done. Add rice and stir-fry until heated through. Add sweet soy sauce and salt, mix well. Serve with fried egg on top.",
-    image: "https://i.ibb.co.com/k8BTQRV/079979700-1587487794-Sajiku-1.jpg",
-};
+// const recipe = {
+//     title: "Nasi Goreng (Fried Rice)",
+//     instruction: "Heat oil in a pan. Add garlic, shallots, and chili, sauté until fragrant. Add chicken or shrimp, cook until done. Add rice and stir-fry until heated through. Add sweet soy sauce and salt, mix well. Serve with fried egg on top.",
+//     image: "https://i.ibb.co.com/k8BTQRV/079979700-1587487794-Sajiku-1.jpg",
+// };
 
 // Put to variable to connect to where will the script function and display the recipe
 const getRecipeButton = document.getElementById('getRecipeBtn'); // variable for button click
@@ -13,7 +13,14 @@ const getRecipeContainer = document.getElementById('recipeContainer'); // variab
 // content that will display
 function displayRecipe(recipe) {
     // Hide the button after clicking
-    getRecipeButton.style.display = 'none';
+    // getRecipeButton.style.display = 'none';
+
+    //checking is the recipe available?
+    if(!recipe || !recipe.instruction) {
+        console.error('Recipe is not available');
+        getRecipeContainer.innerHTML = `<p>Failed to load recipe</p>`;
+        return;
+    }
 
     //create order list for recipe
     const instructionsArray = recipe.instruction.split('. ').filter(step => step.trim());
@@ -28,26 +35,34 @@ function displayRecipe(recipe) {
 }
 
 //fetch recipe from the Meal idb API
-async function fetchRandomRecipe (){
+async function fetchRandomRecipe () {
     try {
         const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+        console.log(response); //to debugging
         if (!response.ok) {
             throw new Error('Unavailable Network');
         }
         const data = await response.json();
+        console.log(data);
+
+        //Check the meals and firstMeal
+        if (!data.meals || !data.meals[0]) {
+            throw new Error('No meals found');
+        }
+
         const recipe = {
             title: data.meals[0].strMeal,
-            instruction: data.meals[0].strInstruction,
+            instruction: data.meals[0].strInstructions,
             image: data.meals[0].strMealThumb,
         };
         displayRecipe(recipe);
     } catch (error) {
-        console.error('There is a proble with fetch:', error);
-        getRecipeContainer.innerHTML = '<p>Failed to load recipe.</p>';
+        console.error('There is a problem with fetch:', error);
+        getRecipeContainer.innerHTML = `<p>Failed to load recipe. Please try again later.</p>`;
     }
 }
 
-// Execute with event listener
+// Execute by adding with event listener to the button
 getRecipeButton.addEventListener('click', () => {
     fetchRandomRecipe();
 });
